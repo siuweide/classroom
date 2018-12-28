f3ce414c38362fd2ba0c9bd43a36ead62e99decb
siuweide86 <45276856+siuweide@users.noreply.github.com>
2018-12-28 17:21:47 +0800

    Update qiushibaire.py

--- qiushibaire.py
+++ qiushibaire.py
@@ -3,24 +3,20 @@ import re
 import os
 import _thread
 
-def get_image(threadName):
-    url = 'https://www.qiushibaike.com/imgrank/'
+url = 'https://www.qiushibaike.com/imgrank/'
+photo_path = os.path.abspath(os.path.dirname(__file__)) + '/photo/'
+response = requests.get(url)
+rps = response.text
+html_url = re.findall(r'//\w+.*medium.*\.jpg',rps)
 
-    photo_path = os.path.abspath(os.path.dirname(__file__)) + '/photo/'
-    response = requests.get(url)
-    rps = response.text
-    html_url = re.findall(r'//\w+.*medium.*\.jpg',rps)
-    for index,jpg in enumerate(html_url[:10]):
-        url_jpg = 'https:' + jpg
-        response = requests .get(url_jpg)
-        with open(photo_path + 'image'+ str(index+1) + '.jpg','wb') as file:
-            print('%s: 正在下载图片...' % threadName)
-            for data in response.iter_content(128):
-                file.write(data)
-try:
-    _thread.start_new_thread(get_image, ('Thread-1',))
-    _thread.start_new_thread(get_image, ('Thread-2',))
-except:
-    print("Error: unable to start thread")
+def get_image(html_url):
+    jpg_url = 'http:%s' %html_url
+    response = requests.get(jpg_url)
+    with open(photo_path + jpg_url.split('/')[-1],'wb') as f:
+        f.write(response.content)
+        print(jpg_url.split('/')[-1] + '图片下载完成')
+
+for i in html_url:
+    _thread.start_new_thread(get_image,(i,))
 
 input('press Enter to exit...\n')
