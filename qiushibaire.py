fddd9ed7b0f5c9ac3d680fbc77d746adf80701d9
siuweide86 <45276856+siuweide@users.noreply.github.com>
2018-12-28 17:44:14 +0800

    Update qiushibaire.py

--- qiushibaire.py
+++ qiushibaire.py
@@ -5,11 +5,14 @@ import _thread
 
 url = 'https://www.qiushibaike.com/imgrank/'
 photo_path = os.path.abspath(os.path.dirname(__file__)) + '/photo/'
+if not os.path.exists(photo_path):
+    os.makedirs(photo_path)
 response = requests.get(url)
 rps = response.text
 html_url = re.findall(r'//\w+.*medium.*\.jpg',rps)
 
 def get_image(html_url):
+    global data
     jpg_url = 'http:%s' %html_url
     response = requests.get(jpg_url)
     with open(photo_path + jpg_url.split('/')[-1],'wb') as f:
@@ -18,5 +21,6 @@ def get_image(html_url):
 
 for i in html_url:
     _thread.start_new_thread(get_image,(i,))
+
 
 input('press Enter to exit...\n')
