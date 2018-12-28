79ff3ff30b492ea010c325e0a708b35e496bc4fa
siuweide86 <45276856+siuweide@users.noreply.github.com>
2018-12-28 10:36:02 +0800

    Create qiushibaire.py

---
+++ qiushibaire.py
@@ -0,0 +1,26 @@
+import requests
+import re
+import os
+import _thread
+
+def get_image(threadName):
+    url = 'https://www.qiushibaike.com/imgrank/'
+
+    photo_path = os.path.abspath(os.path.dirname(__file__)) + '/photo/'
+    response = requests.get(url)
+    rps = response.text
+    html_url = re.findall(r'//\w+.*medium.*\.jpg',rps)
+    for index,jpg in enumerate(html_url[:10]):
+        url_jpg = 'https:' + jpg
+        response = requests .get(url_jpg)
+        with open(photo_path + 'image'+ str(index+1) + '.jpg','wb') as file:
+            print('%s: 正在下载图片...' % threadName)
+            for data in response.iter_content(128):
+                file.write(data)
+try:
+    _thread.start_new_thread(get_photo, ('Thread-1',))
+    _thread.start_new_thread(get_photo, ('Thread-2',))
+except:
+    print("Error: unable to start thread")
+
+input('press Enter to exit...\n')
